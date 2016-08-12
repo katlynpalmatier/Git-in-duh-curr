6d0f4287b870235596275c5ede728a27bd3e25f1
Katlyn Palmatier <katlynpalmatier@gmail.com>
2016-08-12 10:23:06 -0400

    do not pus

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var R = 0
+var G = 0
+var B = 0
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0, 0, 0);
+}
+function draw() {
+    fill(R, G, B);
+    R = random(255);
+    G = 30;
+    B = 150;
+    noStroke();
+    ellipse(mouseX, mouseY, 13, 13);
+    if(mouseIsPressed){
+      noStroke();
+      fill(0, 0, 0);
+      ellipse(mouseX, mouseY, 16, 16);
+    }
+  
+
+}
